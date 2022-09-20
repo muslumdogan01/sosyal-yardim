3f79e86ce644d811b36f9dbdded9220600319230
muslumdogan01 <muslumdogan01@gmail.com>
2022-09-20 09:33:06 +0300

    some fix

--- components/slider.js
+++ components/slider.js
@@ -1,17 +1,46 @@
-export default function Slider() {
+import ReactBeforeSliderComponent from "react-before-after-slider-component";
+import "react-before-after-slider-component/dist/build.css";
+import Image from "next/image";
+import { Dialog } from "@headlessui/react";
+
+
+const FIRST_IMAGE = {
+  imageUrl: "https://example.com/.../some-image.jpg",
+};
+const SECOND_IMAGE = {
+  imageUrl: "https://example.com/.../some-image-2.jpg",
+};
+
+<ReactBeforeSliderComponent
+  firstImage={FIRST_IMAGE}
+  secondImage={SECOND_IMAGE}
+/>;
+
+export default function Slider({isOpen,setIsOpen}) {
   return (
-    <div id="comparison">
-      <figure>
-        <div id="divisor"></div>
-      </figure>
-      <input
-        type="range"
-        min="0"
-        max="100"
-        value="50"
-        id="slider"
-        oninput="moveDivisor()"
-      />
-    </div>
+    <Dialog
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      className="relative z-50"
+    >
+      {/* The backdrop, rendered as a fixed sibling to the panel container */}
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+      {/* Full-screen scrollable container */}
+      <div className="fixed inset-0 overflow-y-auto">
+        {/* Container to center the panel */}
+        <div className="flex min-h-full items-center justify-center p-4">
+          {/* The actual dialog panel  */}
+          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
+            <Dialog.Title>Complete your order</Dialog.Title>
+            
+            <div className="">
+            <Image  src={FIRST_IMAGE} className="p-20 m-20" width={350} height={300} />
+            </div>
+           
+          </Dialog.Panel>
+        </div>
+      </div>
+    </Dialog>
   );
 }
--- pages/support.js
+++ pages/support.js
@@ -1,14 +1,23 @@
-import Image from "next/image"
-
+import ImageSlider from "react-image-comparison-slider";
+import Image from "next/image";
 
 export default function Support() {
   return (
-    <div className="relative w-full">
-              <div className="absolute ">
-           
-            <Image src="/bg.png" width={1440} height={120} />
-         
-          </div>
+    <div style={{ width: 700, height: 450 }}>
+      <ImageSlider
+          image1={<Image src="/gallery2.png" width={500} height={500} />}
+          
+        onSlide={() => {
+          console.log("sliding");
+        }}
+      />
     </div>
-  )
+  );
+}
+{
+  /* <Image src={FIRST_IMAGE} width="300" height="500"/> */
+}
+
+{
+  /* <Image src="/bg.png" width={1440} height={120} /> */
 }
