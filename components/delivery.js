51851823370b87a6c02c4c02c118f0031758fcb3
muslumdogan01 <muslumdogan01@gmail.com>
2022-09-24 23:37:27 +0300

    some fixx

--- components/delivery.js
+++ components/delivery.js
@@ -4,84 +4,84 @@ import React from "react";
 export default function Delivery() {
   return (
     <section class="mb-12">
-        <div class="bg-white alert alert-dismissible rounded-lg py-8 px-6 text-black md:flex justify-between items-center text-center md:text-left">
-          <div className="w-auto flex justify-center items-center flex-col md:flex-row">
-            <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start relative md:mr-32">
-              <span className="w-14 md:absolute md:left-0 ">
-                <img src="/destekDetay1.png" />
-              </span>
-              <span className="w-14 md:absolute md:left-8">
-                <img src="/destekDetay1.png" />
-              </span>
-              <span className="w-14 md:absolute md:left-16 ">
-                <img src="/destekDetay1.png" />
-              </span>
-            </div>
-            <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center  md:justify-start md:mx-[50px]">
-              <div className="flex flex-col">
-                <p className="font-jakarta font-semibold text-xs text-[#57544F]">
-                  AD SOYAD
-                </p>
-                <p className="font-jakarta font-normal text-xl text-black">
-               Tugay Balci
-                </p>
-              </div>
-            </div>
-            <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start">
-              <div className="flex flex-col">
-                <p className="font-jakarta font-semibold text-xs text-[#57544F]">
-                  TELEFON
-                </p>
-                <p className="font-jakarta font-normal text-xl text-black">
-                  05539335203
-                </p>
-              </div>
-            </div>
-            <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start md:mx-[50px]">
-              <div className="flex flex-col">
-                <p className="font-jakarta font-semibold text-xs text-[#57544F]">
-                  TADİLAT DURUMU
-                </p>
-                <p className="font-jakarta font-normal text-xl text-black">
-                  Balkon Tadilatı
-                </p>
-              </div>
-            </div>
-            <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start">
-              <div className="flex flex-col">
-                <p className="font-jakarta font-semibold text-xs text-[#57544F]">
-                  MALİYET
-                </p>
-                <p className="font-jakarta font-normal text-xl text-black ">
-                  8,000Tl
-                </p>
-              </div>
-            </div>
+    <div class="bg-white alert alert-dismissible rounded-lg py-8 px-6 text-black md:flex justify-between items-center text-center md:text-left">
+      <div className="w-auto flex justify-center items-center flex-col md:flex-row">
+        <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start relative md:mr-32">
+          <div className="w-14 md:absolute md:left-0 ">
+          <img src="/destekDetay1.png" className="border-2 border-white rounded-[5px]" />
           </div>
-          <div class="flex items-center justify-center md:justify-end md:w-1/4">
-            <button
-            onClick={()=> router.push("/alterationDetail")}
-            class="inline-block px-5 py-[10px] bg-white border border-[#BBB8B3] text-black font-medium text-sm leading-tight uppercase rounded-[5px]  hover:bg-[#BBB8B3] hover:text-white focus:outline-none transition duration-150 ease-in-out mr-10"
-            href="#!"
-              role="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              Detayı Gör
-            </button>
-            <button
-            onClick={()=> router.push("/support")}
-            class="inline-block px-5 py-[10px] bg-[#E6EEE3] text-black font-medium text-sm leading-tight uppercase rounded-[5px]  hover:bg-[#D4DCD1]  focus:outline-none  transition duration-150 ease-in-out "
-            href="#!"
-              role="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              Destek Ver
-            </button>
-
+          <div className="w-14 md:absolute md:left-8 ">
+          <img src="/destekDetay1.png" className="border-2 border-white rounded-[5px]" />
+          </div>
+          <div className="w-14 md:absolute md:left-16 ">
+          <img src="/destekDetay1.png" className="border-2 border-white rounded-[5px]" />
           </div>
         </div>
-      </section>
+        <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center  md:justify-start md:mx-[50px]">
+          <div className="flex flex-col">
+            <p className="font-jakarta font-semibold text-xs text-[#57544F]">
+              AD SOYAD
+            </p>
+            <p className="font-jakarta font-normal text-xl text-black">
+              Tugay Balci
+            </p>
+          </div>
+        </div>
+        <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start">
+          <div className="flex flex-col">
+            <p className="font-jakarta font-semibold text-xs text-[#57544F]">
+              TELEFON
+            </p>
+            <p className="font-jakarta font-normal text-xl text-black">
+              05539335203
+            </p>
+          </div>
+        </div>
+        <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start md:mx-[50px]">
+          <div className="flex flex-col">
+            <p className="font-jakarta font-semibold text-xs text-[#57544F]">
+              TADİLAT DURUMU
+            </p>
+            <p className="font-jakarta font-normal text-xl text-black">
+              Balkon Tadilatı
+            </p>
+          </div>
+        </div>
+        <div class="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start">
+          <div className="flex flex-col">
+            <p className="font-jakarta font-semibold text-xs text-[#57544F]">
+              MALİYET
+            </p>
+            <p className="font-jakarta font-normal text-xl text-black ">
+              8,000Tl
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center justify-center md:justify-end md:w-1/4">
+        <button
+        onClick={()=> router.push("/alterationDetail")}
+          class="inline-block px-5 py-[10px] bg-white border border-[#BBB8B3] text-black font-medium text-sm leading-tight uppercase rounded-[5px]  hover:bg-[#BBB8B3] hover:text-white focus:outline-none transition duration-150 ease-in-out mr-10"
+          href="#!"
+          role="button"
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+        >
+          Detayı Gör
+        </button>
+        <button
+        onClick={()=> router.push("/support")}
+          class="inline-block px-5 py-[10px] bg-[#E6EEE3] text-black font-medium text-sm leading-tight uppercase rounded-[5px]  hover:bg-[#D4DCD1]  focus:outline-none  transition duration-150 ease-in-out "
+          href="#!"
+          role="button"
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+        >
+          Destek Ver
+        </button>
+
+      </div>
+    </div>
+  </section>
   );
 }
--- components/gallery/index.js
+++ components/gallery/index.js
@@ -5,17 +5,17 @@ export default function Gallery() {
   return (
     <div className="flex md:mt-[8.938rem]">
       <div className="w-full flex md:flex-row flex-col flex-wrap items-center justify-center ">
-        <div className="relative w-64 z-40">
+        <div className="relative w-64 z-40 mb-6 md:mb-0">
           <span className="md:absolute md:border-[7px] md:shadow-xl md:border-white w-full md:left-28 md:-top-5 md:rotate-[-15deg]">
             <img src="/destekDetay1.png" width={242} height={242} />
           </span>
         </div>
-        <div className="relative w-64  z-30">
+        <div className="relative w-64 mb-6 md:mb-0 z-30">
           <span className="md:absolute w-full md:border-[7px] md:shadow-xl md:border-white md:left-10 md:rotate-[9.84deg] ">
             <img src="/destekDetay1.png" width={242} height={242} />
           </span>
         </div>
-        <div className="relative w-64  z-20">
+        <div className="relative w-64 mb-6 md:mb-0 z-20">
           <span className="md:absolute w-full md:border-[7px] md:shadow-xl md:border-white md:-top-5 md:right-30 md:rotate-[-15deg]">
             <img src="/destekDetay1.png" width={242} height={242} />
           </span>
